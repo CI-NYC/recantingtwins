d530fdda81ea4b0169fb528d1572623dc5e1cb8d
Ano <48302151+WeakCha@users.noreply.github.com>
2023-10-30 14:30:26 -0400

    Update ps.R

--- R/ps.R
+++ R/ps.R
@@ -1,4 +1,4 @@
-ps_disc <- function(data, W, A, .control) {
+ps <- function(data, W, A, .control) {
     out <- matrix(nrow = nrow(data), ncol = 2)
     colnames(out) <- c("P(0|W)", "P(1|W)")
     fit <- mlr3superlearner(data = data[, c(A, W), with = FALSE],
@@ -9,8 +9,6 @@ ps_disc <- function(data, W, A, .control) {
     
     out[, "P(1|W)"] <- predict(fit, data[,c(W), with = F], discrete = F)
     out[, "P(0|W)"] <- 1 - out[, "P(1|W)"]
-    
-    #browser()
     
     list(pred = out,
         fit = fit)
