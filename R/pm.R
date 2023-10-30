2c936c805dc828e2a50bebb23a397dedef616aad
Ano <48302151+WeakCha@users.noreply.github.com>
2023-10-30 14:29:00 -0400

    Update pm.R

--- R/pm.R
+++ R/pm.R
@@ -1,66 +1,60 @@
-pm1_disc <- function(data, A, W, M, .control) {
+pm1 <- function(data, A, W, M, .control) {
   out <- matrix(nrow = nrow(data), ncol = 2)
   colnames(out) <- c("P(M|0,W)", "P(M|1,W)")
   
-  #formula = paste0("as.factor(", M, ") ~ ", paste(W, collapse = "+"), " + ", A)
-  #rf = multinom(as.formula(formula), data=data)
-  
-  rf <- mlr3superlearner(data[,c(A, W, M), with = FALSE],
+  fit <- mlr3superlearner(data[,c(A, W, M), with = FALSE],
                           target = M, 
                           list(list("multinom", trace = FALSE), "lightgbm", "rpart"), 
                           outcome_type = "multiclass")
   
-  data_temp = assign_value(data, A, 0)
+  data_temp <- assign_value(data, A, 0)
   
-  `P(M|0,W)` = predict(rf, data_temp[,c(A, W), with = F], discrete = F)
+  `P(M|0,W)` <- predict(rf, data_temp[,c(A, W), with = F], discrete = F)
   
-  data_temp = assign_value(data, A, 1)
+  data_temp <- assign_value(data, A, 1)
   
-  `P(M|1,W)` = predict(rf, data_temp[,c(A, W), with = F], discrete = F)
+  `P(M|1,W)` <- predict(rf, data_temp[,c(A, W), with = F], discrete = F)
   
-  M_data = as.vector(as.matrix(data[,c(M), with = F]))
-  mask_M = one_hot(as.data.table(as.factor(M_data)))
+  M_data <- as.vector(as.matrix(data[,c(M), with = F]))
+  mask_M <- one_hot(as.data.table(as.factor(M_data)))
   
-  `P(M|0,W)` = rowSums(`P(M|0,W)` * mask_M)
-  `P(M|1,W)` = rowSums(`P(M|1,W)` * mask_M)
+  `P(M|0,W)` <- rowSums(`P(M|0,W)` * mask_M)
+  `P(M|1,W)` <- rowSums(`P(M|1,W)` * mask_M)
   
   out[, "P(M|0,W)"] <- `P(M|0,W)`
   out[, "P(M|1,W)"] <- `P(M|1,W)`
   
   list(pred = out,
-        fit = rf)
+       fit = fit)
 }
 
-pm2_disc <- function(data, A, W, M, Z, .control) {
+pm2 <- function(data, A, W, M, Z, .control) {
   
-  out <- matrix(nrow = nrow(data), ncol = 2)
+  out <- matrix(nrow <- nrow(data), ncol = 2)
   colnames(out) <- c("P(M|0,Z,W)", "P(M|1,Z,W)")
   
-  #formula = paste0("as.factor(", M, ") ~ ", Z, " + ", paste(W, collapse = "+"), " + ", A)
-  #rf = multinom(as.formula(formula), data=data)
-  
-  rf <- mlr3superlearner(data[,c(A, W, Z, M), with = FALSE],
+  fit <- mlr3superlearner(data[,c(A, W, Z, M), with = FALSE],
                           target = M, 
                           list(list("multinom", trace = FALSE), "lightgbm", "rpart"), 
                           outcome_type = "multiclass")
   
-  data_temp = assign_value(data, A, 0)
+  data_temp <- assign_value(data, A, 0)
   
-  `P(M|0,Z,W)` = predict(rf, data_temp[,c(A, W, Z), with = F], discrete = F)
+  `P(M|0,Z,W)` <- predict(rf, data_temp[,c(A, W, Z), with = F], discrete = F)
   
-  data_temp = assign_value(data, A, 1)
+  data_temp <- assign_value(data, A, 1)
   
-  `P(M|1,Z,W)` = predict(rf, data_temp[,c(A, W, Z), with = F], discrete = F)
+  `P(M|1,Z,W)` <- predict(rf, data_temp[,c(A, W, Z), with = F], discrete = F)
   
-  M_data = as.vector(as.matrix(data[,c(M), with = F]))
-  mask_M = one_hot(as.data.table(as.factor(M_data)))
+  M_data <- as.vector(as.matrix(data[,c(M), with = F]))
+  mask_M <- one_hot(as.data.table(as.factor(M_data)))
   
-  `P(M|0,Z,W)` = rowSums(`P(M|0,Z,W)` * mask_M)
-  `P(M|1,Z,W)` = rowSums(`P(M|1,Z,W)` * mask_M)
+  `P(M|0,Z,W)` <- rowSums(`P(M|0,Z,W)` * mask_M)
+  `P(M|1,Z,W)` <- rowSums(`P(M|1,Z,W)` * mask_M)
   
   out[, "P(M|0,Z,W)"] <- `P(M|0,Z,W)`
   out[, "P(M|1,Z,W)"] <- `P(M|1,Z,W)`
   
   list(pred = out,
-        fit = rf)
+       fit = fit)
 }
